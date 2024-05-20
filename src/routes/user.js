4f556c7641924058e5d58b703088f946befeb7b4
Villi5ta <stankvilius@gmail.com>
2024-05-20 15:44:36 +0300

    Basic log in / sign up added

---
+++ src/routes/user.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { SIGN_UP, LOG_IN } from "../controllers/user.js";
+
+const router = express.Router();
+
+router.post("/users/signup", SIGN_UP);
+router.post("/users/login", LOG_IN);
+
+export default router;
